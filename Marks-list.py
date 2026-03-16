d65f6043cafd2bbace88e8a387b4682af9497559
Gitansh Agalcha <gitanshagalcha@gmail.com>
2026-03-16 10:01:40 +0530

    Add files via upload

---
+++ Marks-list.py
@@ -0,0 +1,27 @@
+#Write a program to accept marks of 6 students and display them in a sorted manner.
+
+import os 
+os.system('cls' if os.name == 'nt' else 'clr')
+
+marks = []
+
+m1 = int(input("Enter Your Marks: "))
+marks.append(m1)
+
+m2 = int(input("Enter Your Marks: "))
+marks.append(m2)
+
+m3 = int(input("Enter Your Marks: "))
+marks.append(m3)
+
+m4 = int(input("Enter Your Marks: "))
+marks.append(m4)
+
+m5 = int(input("Enter Your Marks: "))
+marks.append(m5)
+
+m6 = int(input("Enter Your Marks: "))
+marks.append(m6)
+
+marks.sort()
+print(marks)
---
+++ list.py
@@ -0,0 +1,5 @@
+# Write a program to sum a list with 4 numbers
+
+List = [3, 2, 56, 34]
+
+print(sum(List))
